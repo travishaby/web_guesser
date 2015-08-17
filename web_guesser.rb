69d6ee6bc79c5a0beeadd11a0175cad46ed8e28e
Travis Haby <travishaby@gmail.com>
2015-08-17 15:12:24 -0500

    guesses limited to 5, colors correspond to result of guessing, turns white and ends the game after five guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,24 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(0..100)
- 
+
 def responses(response)
-  responses = {too_high: "Too high!", too_low: "Too low!", correct: "You are correct. Nice work!"}
+  responses = {too_high: "Too high!", too_low: "Too low!", correct: "You are correct. Nice work!", out_of_guesses: "Sorry, you're out of guesses!"}
   responses[response]
 end
 
 def guesses
-  @guesses ||= []
+  @@guesses ||= []
+end
+
+def answer
+  "The secret number is #{SECRET_NUMBER}"
 end
 
 def check_guess(guess)
+  return responses(:out_of_guesses) if guesses.size > 4
   if guess.to_i > SECRET_NUMBER
     guesses << guess
     responses(:too_high)
   elsif guess.to_i < SECRET_NUMBER
     guesses << guess
     responses(:too_low)
-  else
+  else guess.to_i == SECRET_NUMBER
     responses(:correct)
   end
 end
