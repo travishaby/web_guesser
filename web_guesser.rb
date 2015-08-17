07922d49bbae19052b0c4245850ed42041de369a
Travis Haby <travishaby@gmail.com>
2015-08-17 13:55:16 -0500

    get '/' do     guess = params[guess]     response = check_guess(guess)     erb :index, :locals => {:response => response, :guesses => guesses}

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+SECRET_NUMBER = rand(0..100)
 
-random_number = rand(0..100)
+def responses(response)
+  responses = {too_high: "Too high!", too_low: "Too low!", correct: "You are correct. Nice work!"}
+  responses[response]
+end
+
+def guesses
+  @guesses ||= []
+end
+
+def check_guess(guess)
+  if guess.to_i > SECRET_NUMBER
+    guesses << guess
+    responses(:too_high)
+  elsif guess.to_i < SECRET_NUMBER
+    guesses << guess
+    responses(:too_low)
+  else
+    responses(:correct)
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:random_number => random_number}
+  guess = params["guess"]
+  response = check_guess(guess)
+  erb :index, :locals => {:response => response, :guesses => guesses}
 end
